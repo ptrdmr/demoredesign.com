c123976ec287b5e68a75816eb99b0047c741b1cb
ptrdmr <107432230+ptrdmr@users.noreply.github.com>
2024-10-07 23:04:55 -0700

    re-commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+      appDir: true, // This enables the App Router (new directory-based routing)
+    },
+  };
+  
+  export default nextConfig;
+  
